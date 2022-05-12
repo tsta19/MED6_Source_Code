65d189f88cbe12c262ac3c4832186cca440e5586
tsta19 <tsta19@student.aau.dk>
2022-05-12 11:02:55 +0200

    Added comments to Kmeans.py

--- Source_Code/Kmeans.py
+++ Source_Code/Kmeans.py
@@ -12,17 +12,23 @@ class Kmeans:
     imdir = "Source_Code//DATA//DATA_BOTH//"
     targetdir = "Source_Code//DATA//SAVE_DIRECTORY"
 
+    # Find all images in directory with filetypes .jpg and .jpeg
     filelist = glob.glob(os.path.join(imdir, ('*.jpg' or '*.jpeg')))
+    # Sort all images in ascending order
     filelist.sort()
 
     def clustering(self, features, img, verbose: bool):
         if verbose:
             print("Kmeans: Running function -> 'clustering' ...")
         # Clustering
+
+        # Run kmeans function with 2 defined clusters with ++ initialisation scheme on the featurearray: features
         kmeans = KMeans(n_clusters=2, random_state=0, init="k-means++").fit(np.array(features))
 
         # Save image to directory
         print("\n")
+
+        # For every image depending on the kmeans.label_ separate them into their respective folders DATA -> SAVE_DIRECTORY -> CLUSTER_GROUP_01 AND 02
         for i, clusterGroup in enumerate(kmeans.labels_):
             print("    Copy: %s / %s" % (i, len(kmeans.labels_)), end="\r")
             # fileManager.save_image(targetdir, filelist[i], "cluster_group", str(i))
